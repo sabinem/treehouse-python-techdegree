da25b84c6976d66c2f5c4bb4d5418e15a97a0425
Sabine Maennel <sabine.maennel@gmail.com>
2017-03-20 12:40:05 +0100

    initial commit

---
+++ mineral_catalog/urls.py
@@ -0,0 +1,20 @@
+"""
+mineral_catalog URL Configuration
+"""
+
+from django.conf.urls import url, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+
+
+urlpatterns = [
+    url(r'^', include('minerals.urls', namespace='minerals')),
+]
+
+urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
