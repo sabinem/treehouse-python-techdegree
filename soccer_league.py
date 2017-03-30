578aa778d2056d670d73f41361b699d6dee24b83
Sabine Maennel <sabine.maennel@gmail.com>
2017-03-30 09:10:43 +0200

    some updates according to grading feedback
    removed some comments

--- soccer_league.py
+++ soccer_league.py
@@ -1,3 +1,5 @@
+"""File includes all functions for Building a Soccer League 
+and printing team assigmnet letters for the players"""
 import csv
 import operator
 DRAGONS = 'Dragons'
@@ -9,11 +11,9 @@ PRACTICE_DATETIME = {DRAGONS: 'March 17 at 1pm',
 
 
 def print_letter_to_guardian(teamname, player):
-    """
-    A letter is written to the Guardians about which team the player is in and when practice is starting.
+    """A letter is written to the Guardians about which team the player is in and when practice is starting.
     The letter is written to a file with the name of the players: sabine_maennel.txt is the letter regarding
-    player Sabine Maennel for example.
-    """
+    player Sabine Maennel for example."""
     template = "Dear {},\nwe are happy to inform you that {} made it into team {}.\nThe first soccer " \
                "practice will be on {}.\nwith kind regards\n    The Soccer League"
     letter = template.format(player['Guardian Name(s)'],
@@ -26,55 +26,34 @@ def print_letter_to_guardian(teamname, player):
     return filename
 
 def process_team(teamname, players):
-    """
-    the team is processed: the writing of the letters to the players
-    guardians is triggered here. Also a message is printed about hwo the players
-    have been distributed into the teams.
-    """
+    """the team is processed: the writing of the letters to the players
+    guardians is triggered here. Also a message is printed about how the players
+    have been distributed into the teams."""
     teamcount = 0
     teamheight_sum = 0
     teamcount_experienced = 0
     letters = []
-
-    # process the players in the team
     for player in players:
-
         teamcount += 1
         teamheight_sum += int(player['Height (inches)'])
-
         if player['Soccer Experience'] == 'YES':
             teamcount_experienced += 1
-
-        # the letter is written
         filename = print_letter_to_guardian(teamname, player)
         letters.append(filename)
-
-    # the average team height is calculated
     teamheight_avg = teamheight_sum/teamcount
-
-    # message to the user about the result of the team building
     print("TEAM {} \n  Average height: {}, \n  Players: Total/Experienced {}/{}\n  Letters printed: {}".
           format(teamname, teamheight_avg, teamcount, teamcount_experienced, ', '.join(letters)))
 
 
 if __name__ == "__main__":
-    # read players from csv files
+    """main programm opens the player file and builds the teams so that
+    they have players with balanced experence and height of players"""
     with open('data/soccer_players.csv', newline='\n') as playerfile:
-
-        # read form csv file into a dictionary
         players = csv.DictReader(playerfile, delimiter=',')
-
-        # transform into a list of players
         playerslist = list(players)
-
-        # sort list by players height
         playerslist.sort(key=operator.itemgetter('Height (inches)'))
-
-        # seperate experienced from unexperienced players
         experience = [player for player in playerslist if player['Soccer Experience'] == 'YES']
         noexperience = [player for player in playerslist if player['Soccer Experience'] == 'NO']
-
-        # now slice both lists in a way that height is distributed as equal as possible
         teams = {}
         teams[SHARKS] = experience[::3] + noexperience[2::3]
         teams[RAPTORS] = experience[1::3] + noexperience[1::3]
